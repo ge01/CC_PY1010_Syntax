b934b38d9f0db13e67c4e48b49df074fc6c2c06b
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-10-11 19:55:47 -0500

    python syntax

---
+++ python_syntax_01/python_syntax_01/python_syntax_01.py
@@ -0,0 +1,66 @@
+#Bringing It All Together
+#In a year, Jurgen Klopp has taken Liverpool back to where they
+#should be - now he has to finish the job
+monty = True
+python = 1.234
+monty_python = python ** 2
+
+#Whitespace Means Right Space
+def spam():
+    eggs = 12
+    return eggs
+        
+print (spam())
+
+
+#Welcome!
+print ("Welcome to Python!")
+
+#Break It Down
+#word = input("Input a word in English.")
+
+#Variables
+my_variable = 10
+
+#Booleans
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+#You've Been Reassigned
+# my_int is set to 7 below. What do you think
+# will happen if we reset it to 3 and print the result?
+
+my_int = 7
+
+# Change the value of my_int to 3 on line 8!
+
+my_int = 3
+
+# Here's some code that will print my_int to the console:
+# The print keyword will be covered in detail soon!
+
+print (my_int)
+
+#A Matter of Interpretation
+spam = True
+eggs = False
+
+
+#Multi-Line Comments
+#Liverpool and Spurs are setting a thrilling Premier League Pace
+
+#Math Operations
+count_to = 105 + 222
+print (count_to)
+
+eggs = 10 ** 2
+print (eggs)
+
+spam = 5 % 4
+print (spam)
+
+
+#Review
+
+
